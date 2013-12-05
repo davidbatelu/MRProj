c30750a679c1375eb1a124fc5cd2ba3683900234
David Batelu <dave@stackdriver.com>
2013-12-05 18:50:16 -0500

    K means recursive finished. Removing unwanted stuff

--- java_code/kmeans/src/org/myorg/Kmeans.java
+++ java_code/kmeans/src/org/myorg/Kmeans.java
@@ -11,6 +11,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
+//import org.apache.hadoop.mapreduce.Counter;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
@@ -22,6 +23,11 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class Kmeans {
 	static int K = 3, features = 2;
+	
+	public enum MyCounters {
+        Count
+	}
+	
 	public static final double measureDistance(Double[] center, Double[]  v) {
 	  double sum = 0;
 	  // Ignore last label
@@ -33,15 +39,15 @@ public class Kmeans {
 	}
 	
 	@SuppressWarnings("deprecation")
-	public static Double[][] load_centers(FileSystem fs) throws IOException {
+	public static Double[][] load_centers(FileSystem fs, String subPath) throws IOException {
         String line;
         Double[][] temp_centers = new Double[K][];
 //        System.out.print("In load_centers\n");
-		FSDataInputStream cache = fs.open(new Path("/user/dave/centers.txt"));
+		FSDataInputStream cache = fs.open(new Path("/user/dave/" + subPath  + "/part-r-00000"));
 	   try {
 		   int i = 0;
 		   while((line = cache.readLine()) != null ){
-               String[] parts = line.trim().split(" ");
+               String[] parts = line.trim().split(" |\t");
                temp_centers[i] = new Double[parts.length];
                for (int j = 0; j < parts.length; j++) {
             	   temp_centers[i][j] = Double.parseDouble(parts[j]);
@@ -57,22 +63,22 @@ public class Kmeans {
 	}
 	
 	public static class KMap extends Mapper<LongWritable, Text, IntWritable, Text> {
-		static Double[][] centers;
+		static Double[][] centers;	
 		public void setup(Context context) throws IOException {
-//			   System.out.print("In setup\n");
+			   System.out.print("In setup\n");
 			   FileSystem fs;
 			   try {
 					fs = FileSystem.get(new URI("/user/dave"),context.getConfiguration());
-					centers = load_centers(fs);
+					centers = load_centers(fs, context.getConfiguration().get("my.centers.path"));
 			   } catch (URISyntaxException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 			   }
-//			   System.out.print(centers[0][0] + "," + centers[0][1] + "," + centers[1][0] + "," + centers[1][1] + "\n");
+			   System.out.print(centers[0][0] + "," + centers[0][1] + "," + centers[1][0] + "," + centers[1][1] + "\n");
 		   }
 		
 		public void map(LongWritable key, Text value, Context context) throws InterruptedException, IOException {
-			String[] parts = value.toString().split(" ");
+			String[] parts = value.toString().split(" |\t");
 			Double[] row = new Double[parts.length];
 			int i = 0, minIdx = -1;
 			double min = 99999999.0;
@@ -87,12 +93,63 @@ public class Kmeans {
 					minIdx = c;
 				}
 			}
-			context.write(new IntWritable(minIdx), value);
+			context.write(new IntWritable(minIdx), new Text("1 " + value.toString()));
 		}
+		
 	}
+	
+	
+	
 	public static void initialize(Double[] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			arr[i] = 0.0;
+		}
+	}
+	
+	public static class KComb extends Reducer<IntWritable, Text, IntWritable, Text> {
+//		static Double[][] centers;
+//		public void setup(Context context) throws IOException {
+//			   System.out.print("In setup\n");
+//			   FileSystem fs;
+//			   try {
+//					fs = FileSystem.get(new URI("/user/dave"),context.getConfiguration());
+//					centers = load_centers(fs);
+//			   } catch (URISyntaxException e1) {
+//					// TODO Auto-generated catch block
+//					e1.printStackTrace();
+//			   }
+//			   System.out.print(centers[0][0] + "," + centers[0][1] + "," + centers[1][0] + "," + centers[1][1] + "\n");
+//		}
+		public void reduce(IntWritable key, Iterable<Text> values, Context context) 
+			     throws IOException, InterruptedException {
+			System.out.print("In combine: " + key.toString() + " with values = " + values.toString() + "\n");
+			
+			long num = 0;
+			Double[] feature_sum = new Double[features + 1];
+			initialize(feature_sum);
+			for (Text value : values) {
+//				System.out.print(num + value.toString() + "\n");
+				String[] parts = value.toString().split(" |\t");
+//				System.out.print(num + value.toString() + "\n");
+				int i = 0;
+				for (String part : parts) {
+					if (i == 0) {
+						num += Long.parseLong(part);
+						i++;
+						continue;
+					}
+//					System.out.print(part + "\n");
+//					System.out.print(feature_sum[i] + "\n");
+					feature_sum[i-1] += Double.parseDouble(part);
+					i++;
+				}
+				
+			}
+			String feature_str = num + " ";
+			for (Double feature : feature_sum) {
+				feature_str += feature.toString() + " ";
+			}
+			context.write(key, new Text(feature_str.substring(0, feature_str.length()-1)));
 		}
 	}
 	
@@ -114,21 +171,25 @@ public class Kmeans {
 			     throws IOException, InterruptedException {
 			System.out.print("In reduce: " + key.toString() + " with values = " + values.toString() + "\n");
 			
-			int num = 0;
+			long num = 0;
 			Double[] feature_sum = new Double[features + 1];
 			initialize(feature_sum);
 			for (Text value : values) {
-//				System.out.print(num + value.toString() + "\n");
-				String[] parts = value.toString().split(" ");
-//				System.out.print(num + value.toString() + "\n");
+				System.out.print(num + " " + value.toString() + "\n");
+				String[] parts = value.toString().split(" |\t");
+				System.out.print(num + " " + value.toString() + "\n");
 				int i = 0;
 				for (String part : parts) {
-//					System.out.print(part + "\n");
-//					System.out.print(feature_sum[i] + "\n");
-					feature_sum[i] += Double.parseDouble(part);
+					if (i == 0) {
+						num += Long.parseLong(part);
+						i++;
+						continue;
+					}
+					System.out.print(part + "\n");
+					System.out.print(feature_sum[i-1] + "\n");
+					feature_sum[i-1] += Double.parseDouble(part);
 					i++;
 				}
-				num += 1;
 			}
 			System.out.print("Done with sums: " + key.toString() + " with total :" + num + "\n");
 			int i = 0;
@@ -142,28 +203,43 @@ public class Kmeans {
 		}
 	}
 	
+	public static boolean converged(long counter) {
+//		if (counter <= 2) {
+//			return false;
+//		} else {
+//			
+//		}
+		return false;
+	}
+	
 	public static void main(String[] args) throws Exception {
-		 Configuration conf = new Configuration();
-
-		 Job job = new Job(conf, "kmeans");
-		 job.setOutputKeyClass(Text.class);
-		 job.setOutputValueClass(Text.class);
-		 job.setMapperClass(KMap.class);
-		 job.setReducerClass(KRed.class);
-		 job.setMapOutputKeyClass(IntWritable.class);
-		 job.setMapOutputValueClass(Text.class);
-		 job.setInputFormatClass(TextInputFormat.class);
-		 job.setOutputFormatClass(TextOutputFormat.class);
-//		 job.setCombinerClass(CFRed.class);
-//					 job.setPartitionerClass(WordPartitioner.class);
-//					 job.setNumReduceTasks(5);
-		 
-		 job.setJarByClass(Kmeans.class);
-
-	     FileInputFormat.addInputPath(job, new Path(args[0]));
-	     FileOutputFormat.setOutputPath(job, new Path(args[1]));
-//	     FileInputFormat.addInputPath(job, new Path("/Users/dave/proj/MRProj/output/build_mat/1385316530/part-r-00000"));
-//	     FileOutputFormat.setOutputPath(job, new Path("/Users/dave/proj/MRProj/output/collab/" + Long.toString(unixTime)));
-	     job.waitForCompletion(true);
+		long counter = 0;
+		long max_itr = 10;
+		while (!converged(counter) && counter < max_itr) {
+			 Configuration conf = new Configuration();
+			 conf.set("my.centers.path", "outp." + String.valueOf(counter));	
+			 Job job = new Job(conf, "kmeans");
+			 job.setOutputKeyClass(Text.class);
+			 job.setOutputValueClass(Text.class);
+			 job.setMapperClass(KMap.class);
+			 job.setReducerClass(KRed.class);
+			 job.setMapOutputKeyClass(IntWritable.class);
+			 job.setMapOutputValueClass(Text.class);
+			 job.setInputFormatClass(TextInputFormat.class);
+			 job.setOutputFormatClass(TextOutputFormat.class);
+			 // The combiner involves extra parsing, so not using it may actually be more efficient
+			 job.setCombinerClass(KComb.class);
+	//					 job.setPartitionerClass(WordPartitioner.class);
+	//					 job.setNumReduceTasks(5);
+			 
+			 job.setJarByClass(Kmeans.class);
+	
+		     FileInputFormat.addInputPath(job, new Path(args[0]));
+		     counter++;
+		     FileOutputFormat.setOutputPath(job, new Path("outp." + counter));
+	//	     FileInputFormat.addInputPath(job, new Path("/Users/dave/proj/MRProj/output/build_mat/1385316530/part-r-00000"));
+	//	     FileOutputFormat.setOutputPath(job, new Path("/Users/dave/proj/MRProj/output/collab/" + Long.toString(unixTime)));
+		     job.waitForCompletion(true);
+		}
 	 }
 }
