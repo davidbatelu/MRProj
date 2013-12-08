a99c8b6f21f3cf01323274c0b53bb6bfb1746ea6
Nischint Ramesh <nischintr@gmail.com>
2013-12-07 20:06:10 -0500

    changed code to run on aws. including help file for args.

--- java_code/collab/src/org/myorg/Collab.java
+++ java_code/collab/src/org/myorg/Collab.java
@@ -1,4 +1,5 @@
 package org.myorg;
+
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
@@ -13,50 +14,56 @@ import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 //import no.uib.cipr.matrix.sparse.SparseVector;
 
 public class Collab {
-	public static class PreMap extends Mapper<LongWritable, Text, Text, IntWritable> {
-		public void map(LongWritable key, Text value, Context context) throws InterruptedException, IOException {
+	public static class PreMap extends
+			Mapper<LongWritable, Text, Text, IntWritable> {
+		public void map(LongWritable key, Text value, Context context)
+				throws InterruptedException, IOException {
 			String string = value.toString();
-//			parts = (user_id, song_id, play_count)
+			// parts = (user_id, song_id, play_count)
 			String[] parts = string.split("\t");
-			context.write(new Text(parts[1]), new IntWritable(Integer.parseInt(parts[2])));
+			context.write(new Text(parts[1]),
+					new IntWritable(Integer.parseInt(parts[2])));
 		}
 	}
-	
-	public static class PreReduce extends Reducer<Text, IntWritable, Text, IntWritable> {
-	   public void reduce(Text key, Iterable<IntWritable> values, Context context) 
-	     throws IOException, InterruptedException {
-	       int sum = 0;
-	       for (IntWritable val : values) {
-	           sum += val.get();
-	       }
-	       context.write(key, new IntWritable(sum));
-	   }
+
+	public static class PreReduce extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable val : values) {
+				sum += val.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
 	}
-	
+
 	public static void main(String[] args) throws Exception {
-	 Configuration conf = new Configuration();
-	 long unixTime = System.currentTimeMillis() / 1000L;
+		Configuration conf = new Configuration();
+		long unixTime = System.currentTimeMillis() / 1000L;
 
-	 Job job = new Job(conf, "collab");
-	 job.setOutputKeyClass(Text.class);
-	 job.setOutputValueClass(IntWritable.class);
-	 job.setMapperClass(PreMap.class);
-	 job.setReducerClass(PreReduce.class);
-	 job.setMapOutputKeyClass(Text.class);
-	 job.setMapOutputValueClass(IntWritable.class);
-	 job.setInputFormatClass(TextInputFormat.class);
-	 job.setOutputFormatClass(TextOutputFormat.class);
-//		 job.setCombinerClass(Reduce.class);
-//		 job.setPartitionerClass(WordPartitioner.class);
-//		 job.setNumReduceTasks(5);
-	 
-	 job.setJarByClass(Collab.class);
+		Job job = new Job(conf, "collab");
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		job.setMapperClass(PreMap.class);
+		job.setReducerClass(PreReduce.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		// job.setCombinerClass(Reduce.class);
+		// job.setPartitionerClass(WordPartitioner.class);
+		// job.setNumReduceTasks(5);
 
-//	     FileInputFormat.addInputPath(job, new Path(args[0]));
-//	     FileOutputFormat.setOutputPath(job, new Path(args[1]));
-     FileInputFormat.addInputPath(job, new Path("../../data/train_triplets_mini.txt"));
-     FileOutputFormat.setOutputPath(job, new Path("../../output/songs_count/" + Long.toString(unixTime)));
-     job.waitForCompletion(true);
-   }
-		
+		job.setJarByClass(Collab.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		// FileInputFormat.addInputPath(job, new
+		// Path("../../data/train_triplets_mini.txt"));
+		// FileOutputFormat.setOutputPath(job, new
+		// Path("../../output/songs_count/" + Long.toString(unixTime)));
+		job.waitForCompletion(true);
+	}
+
 }
--- java_code/collab/src/org/myorg/CollaborativeFiltering.java
+++ java_code/collab/src/org/myorg/CollaborativeFiltering.java
@@ -22,9 +22,9 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
-
 public class CollaborativeFiltering {
-	public static HashMap<String, Integer> vector_add(HashMap<String, Integer> h1, HashMap<String, Integer>h2) {
+	public static HashMap<String, Integer> vector_add(
+			HashMap<String, Integer> h1, HashMap<String, Integer> h2) {
 		HashMap<String, Integer> h3 = new HashMap<String, Integer>();
 		h3.putAll(h1);
 		for (Map.Entry<String, Integer> entry : h2.entrySet()) {
@@ -36,22 +36,24 @@ public class CollaborativeFiltering {
 			} else {
 				h3.put(key, value);
 			}
-			
+
 		}
 		return h3;
 	}
-	
-	public static class CFMap extends Mapper<LongWritable, Text, Text, BytesWritable> {
-		public void map(LongWritable key, Text value, Context context) throws InterruptedException, IOException {
+
+	public static class CFMap extends
+			Mapper<LongWritable, Text, Text, BytesWritable> {
+		public void map(LongWritable key, Text value, Context context)
+				throws InterruptedException, IOException {
 			String string = value.toString();
 			HashMap<String, Integer> h = new HashMap<String, Integer>();
-//			parts = (user_id, song_id, play_count)
+			// parts = (user_id, song_id, play_count)
 			String[] key_value = string.split("\t");
 			String[] parts = key_value[1].split(",");
 			for (String parti : parts) {
 				String[] id_countsi = parti.split(":");
 				Integer counti = new Integer(id_countsi[1]);
-//				global_hash.put(id_countsi[0], counti);
+				// global_hash.put(id_countsi[0], counti);
 				for (String partj : parts) {
 					String[] id_countsj = partj.split(":");
 					h.put(id_countsj[0], new Integer(id_countsj[1]) * counti);
@@ -59,113 +61,158 @@ public class CollaborativeFiltering {
 				ByteArrayOutputStream out = new ByteArrayOutputStream();
 				ObjectOutputStream objOut = new ObjectOutputStream(out);
 				objOut.writeObject(h);
-			    objOut.close();
-				context.write(new Text(id_countsi[0]), new BytesWritable(out.toByteArray()));
+				objOut.close();
+				context.write(new Text(id_countsi[0]),
+						new BytesWritable(out.toByteArray()));
 			}
-//		    context.write(new Text("test"), new Text("123"));
+			// context.write(new Text("test"), new Text("123"));
 		}
 	}
-	
-	@SuppressWarnings("deprecation")
-	public static HashMap<String, Integer> load_hash(FileSystem fs) throws IOException {
-		System.out.print("In load_hash");
-        String line;
-		FSDataInputStream cache = fs.open(new Path("/user/dave/counts.txt"));
-		HashMap<String, Integer> hash = new HashMap<String, Integer>();
-	   try {
-		   while((line = cache.readLine()) != null ){
-               String[] parts = line.trim().split("\t");
-               hash.put(parts[0], new Integer (parts[1]));
-		   }
-	   } catch (IOException e) {
-	                   // TODO Auto-generated catch block
-	            e.printStackTrace();
-	   }
-	   return hash;
-	}
-		
-	public static class CFRed extends Reducer<Text, BytesWritable, Text, Text> {
-	   HashMap<String, Integer> counts;
 
-	   public void setup(Context context) throws IOException {
-		   System.out.print("In setup");
-		   FileSystem fs;
-		   try {
-				fs = FileSystem.get(new URI("/user/dave"),context.getConfiguration());
-				counts = load_hash(fs);
-		   } catch (URISyntaxException e1) {
-				// TODO Auto-generated catch block
-				e1.printStackTrace();
-		   }
-	   }
-	   
-	   public float Similarity(int type, Integer dotij, Integer ni, Integer nj) {
-		   switch (type) {
-		   		case 0:
-		   			// Jaccard
-		   			return (float) dotij / (ni + nj + dotij); 
-		   		default:
-		   			return (float) dotij;
-		   }
-	   }
-		
-	   public void reduce(Text key, Iterable<BytesWritable> values, Context context) 
-	     throws IOException, InterruptedException {
-    	   HashMap<String, Integer> map = new HashMap<String, Integer>();
-	       for (BytesWritable val : values) {
-	    	   ObjectInputStream objIn = new ObjectInputStream(new ByteArrayInputStream(val.getBytes()));
-	    	   try {
+	@SuppressWarnings("deprecation")
+	public static HashMap<String, Integer> load_hash(FileSystem fs)
+			throws IOException {
+		System.out.print("In load_hash");
+		String line;
+		// FSDataInputStream cache = fs.open(new Path("/user/dave/counts.txt"));
+		//FSDataInputStream cache = fs.open(new Path(
+		//		"/Users/nischintr/output2/part-r-00000"));
+		FSDataInputStream cache = fs.open(new Path("s3n://mrproj/input/op2"));
+		HashMap<String, Integer> hash = new HashMap<String, Integer>();
+		try {
+			while ((line = cache.readLine()) != null) {
+				String[] parts = line.trim().split("\t");
+				hash.put(parts[0], new Integer(parts[1]));
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return hash;
+	}
+
+	public static class CFCom extends
+			Reducer<Text, BytesWritable, Text, BytesWritable> {
+		public void reduce(Text key, Iterable<BytesWritable> values,
+				Context context) throws IOException, InterruptedException {
+			HashMap<String, Integer> map = new HashMap<String, Integer>();
+			for (BytesWritable val : values) {
+				ObjectInputStream objIn = new ObjectInputStream(
+						new ByteArrayInputStream(val.getBytes()));
+				try {
 					@SuppressWarnings("unchecked")
-					HashMap<String, Integer> actual = (HashMap<String, Integer>) objIn.readObject();
+					HashMap<String, Integer> actual = (HashMap<String, Integer>) objIn
+							.readObject();
 					map = vector_add(actual, map);
-			   } catch (ClassNotFoundException e) {
+				} catch (ClassNotFoundException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-			   }   
-	       }
-//	       String str = new String();
-	       HashMap<String, String> S = new HashMap<String, String>();
-    	   DecimalFormat d = new DecimalFormat("#.#####");
-	       for (Map.Entry<String, Integer> entry : map.entrySet()) {
-	    	   String mapkey = (String) entry.getKey();
-	    	   Integer value = (Integer) entry.getValue();
-	    	   // This may be redundant for now but will be required later when we have values other thank just 1
-	    	   // Have to do an actual similarity function here.
-	    	   // Figure out how we'll get the norms
-	    	   Float tmpval = Similarity(0, value, counts.get(key.toString()), counts.get(mapkey));
-	    	   S.put(mapkey, d.format(tmpval));
-//	    	   str += mapkey + ":" + tmpval.toString() + ",";
-	       }
-	       // String will definitely contain something as a user cannot exists who has not listened to a song
-	       // TODO: Remove curly braces from start and end
-	       String str = S.toString();
-	       context.write(key, new Text(str.substring(1, str.length()-1)));
-	   }
+				}
+			}
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			ObjectOutputStream objOut = new ObjectOutputStream(out);
+			objOut.writeObject(map);
+			objOut.close();
+			context.write(key, new BytesWritable(out.toByteArray()));
+		}
 	}
-	
+
+	public static class CFRed extends Reducer<Text, BytesWritable, Text, Text> {
+		HashMap<String, Integer> counts;
+
+		public void setup(Context context) throws IOException {
+			System.out.print("In setup");
+			FileSystem fs;
+			try {
+				// fs = FileSystem.get(new
+				// URI("/user/dave"),context.getConfiguration());
+				// fs = FileSystem.get(new URI("/Users/nischintr"),
+				//		context.getConfiguration());
+				fs = FileSystem.get(new URI("s3n://mrproj"),
+								context.getConfiguration());
+				counts = load_hash(fs);
+			} catch (URISyntaxException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		}
+
+		public float Similarity(int type, Integer dotij, Integer ni, Integer nj) {
+			switch (type) {
+			case 0:
+				// Jaccard
+				return (float) dotij / (ni + nj + dotij);
+			default:
+				return (float) dotij;
+			}
+		}
+
+		public void reduce(Text key, Iterable<BytesWritable> values,
+				Context context) throws IOException, InterruptedException {
+			HashMap<String, Integer> map = new HashMap<String, Integer>();
+			for (BytesWritable val : values) {
+				ObjectInputStream objIn = new ObjectInputStream(
+						new ByteArrayInputStream(val.getBytes()));
+				try {
+					@SuppressWarnings("unchecked")
+					HashMap<String, Integer> actual = (HashMap<String, Integer>) objIn
+							.readObject();
+					map = vector_add(actual, map);
+				} catch (ClassNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			// String str = new String();
+			HashMap<String, String> S = new HashMap<String, String>();
+			DecimalFormat d = new DecimalFormat("#.#####");
+			for (Map.Entry<String, Integer> entry : map.entrySet()) {
+				String mapkey = (String) entry.getKey();
+				Integer value = (Integer) entry.getValue();
+				// This may be redundant for now but will be required later when
+				// we have values other thank just 1
+				// Have to do an actual similarity function here.
+				// Figure out how we'll get the norms
+				Float tmpval = Similarity(0, value, counts.get(key.toString()),
+						counts.get(mapkey));
+				S.put(mapkey, d.format(tmpval));
+				// str += mapkey + ":" + tmpval.toString() + ",";
+			}
+			// String will definitely contain something as a user cannot exists
+			// who has not listened to a song
+			// TODO: Remove curly braces from start and end
+			String str = S.toString();
+			context.write(key, new Text(str.substring(1, str.length() - 1)));
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
-	 Configuration conf = new Configuration();
-//	 long unixTime = System.currentTimeMillis() / 1000L;
+		Configuration conf = new Configuration();
+		// long unixTime = System.currentTimeMillis() / 1000L;
 
-	 Job job = new Job(conf, "cf");
-	 job.setOutputKeyClass(Text.class);
-	 job.setOutputValueClass(Text.class);
-	 job.setMapperClass(CFMap.class);
-	 job.setReducerClass(CFRed.class);
-	 job.setMapOutputKeyClass(Text.class);
-	 job.setMapOutputValueClass(BytesWritable.class);
-	 job.setInputFormatClass(TextInputFormat.class);
-	 job.setOutputFormatClass(TextOutputFormat.class);
-//	 job.setCombinerClass(CFRed.class);
-//				 job.setPartitionerClass(WordPartitioner.class);
-//				 job.setNumReduceTasks(5);
-	 
-	 job.setJarByClass(CollaborativeFiltering.class);
+		Job job = new Job(conf, "cf");
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		job.setMapperClass(CFMap.class);
+		job.setReducerClass(CFRed.class);
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(BytesWritable.class);
+		job.setInputFormatClass(TextInputFormat.class);
+		job.setOutputFormatClass(TextOutputFormat.class);
+		job.setCombinerClass(CFCom.class);
+		
+		// job.setPartitionerClass(WordPartitioner.class);
+		// job.setNumReduceTasks(5);
 
-     FileInputFormat.addInputPath(job, new Path(args[0]));
-     FileOutputFormat.setOutputPath(job, new Path(args[1]));
-//     FileInputFormat.addInputPath(job, new Path("/Users/dave/proj/MRProj/output/build_mat/1385316530/part-r-00000"));
-//     FileOutputFormat.setOutputPath(job, new Path("/Users/dave/proj/MRProj/output/collab/" + Long.toString(unixTime)));
-     job.waitForCompletion(true);
-   }
+		job.setJarByClass(CollaborativeFiltering.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		// FileInputFormat.addInputPath(job, new
+		// Path("/Users/dave/proj/MRProj/output/build_mat/1385316530/part-r-00000"));
+		// FileOutputFormat.setOutputPath(job, new
+		// Path("/Users/dave/proj/MRProj/output/collab/" +
+		// Long.toString(unixTime)));
+		job.waitForCompletion(true);
+	}
 }
